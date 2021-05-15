2fd1c26de7667f5b2e07a69ff71e229122f04e0f
DMadonko <72002063+DMadonko@users.noreply.github.com>
2021-05-15 18:34:11 +1000

    Heroku files initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+# import necessary libraries
+from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+from scipy import stats
+from scipy.stats import randint
+# Models
+# Tree Classifier
+from sklearn.tree import DecisionTreeClassifier
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.model_selection import RandomizedSearchCV
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+ 
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+XXXXX = create_classes(db)
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# Query the database and send the jsonified results
