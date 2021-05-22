10eb73f739ee69341d381dc0a1a1dc5a13472d9b
Jay <j.sutton@vic.chariot.net.au>
2021-05-23 00:25:56 +1000

    Website is working as intended for final submission on local server

--- static/js/app.js
+++ static/js/app.js
@@ -30,53 +30,53 @@ function doCheckCustomer(event) {
     let avg_utilization_ratio = d3.select("#validationDefault19").node().value;
 
 
-    // let customer = {
-    //     "Customer_Age": parseInt(cust_age),
-    //     "Gender": parseInt(gender),
-    //     "Dependent_count": parseInt(dependants),
-    //     "Education_Level": parseInt(education_level),
-    //     "Marital_Status": parseInt(marital_status),
-    //     "Income_Category": parseInt(income),
-    //     "Card_Category": parseInt(card_cat),
-    //     "Months_on_book": parseInt(months_on_book),
-    //     "Total_Relationship_Count": parseInt(total_Relationship_Count),
-    //     "Months_Inactive_12_mon": parseInt(months_inactive_12_mon),
-    //     "Contacts_Count_12_mon": parseInt(contacts_Count_12_mon),
-    //     "Credit_Limit": parseInt(credit_Limit),
-    //     "Total_Revolving_Bal": parseInt(total_revolving_bal),
-    //     "Avg_Open_To_Buy": parseInt(avg_open_to_buy),
-    //     "Total_Amt_Chng_Q4_Q1": parseFloat(total_amt_chng_Q4_Q1),
-    //     "Total_Trans_Amt": parseInt(total_trans_amt),
-    //     "Total_Trans_Ct": parseInt(total_trans_ct),
-    //     "Total_Ct_Chng_Q4_Q1": parseFloat(total_ct_chng_Q4_Q1),
-    //     "Avg_Utilization_Ratio": parseFloat(avg_utilization_ratio),
+    let customer = {
+        "Customer_Age": parseInt(cust_age),
+        "Gender": parseInt(gender),
+        "Dependent_count": parseInt(dependants),
+        "Education_Level": parseInt(education_level),
+        "Marital_Status": parseInt(marital_status),
+        "Income_Category": parseInt(income),
+        "Card_Category": parseInt(card_cat),
+        "Months_on_book": parseInt(months_on_book),
+        "Total_Relationship_Count": parseInt(total_Relationship_Count),
+        "Months_Inactive_12_mon": parseInt(months_inactive_12_mon),
+        "Contacts_Count_12_mon": parseInt(contacts_Count_12_mon),
+        "Credit_Limit": parseInt(credit_Limit),
+        "Total_Revolving_Bal": parseInt(total_revolving_bal),
+        "Avg_Open_To_Buy": parseInt(avg_open_to_buy),
+        "Total_Amt_Chng_Q4_Q1": parseFloat(total_amt_chng_Q4_Q1),
+        "Total_Trans_Amt": parseInt(total_trans_amt),
+        "Total_Trans_Ct": parseInt(total_trans_ct),
+        "Total_Ct_Chng_Q4_Q1": parseFloat(total_ct_chng_Q4_Q1),
+        "Avg_Utilization_Ratio": parseFloat(avg_utilization_ratio),
 
-    // }
+    }
 
     // this is testing code so I don't have to keep entering values.
     // Remember comment this out and to re-add commented code above
-    let customer = {
-        "Customer_Age": parseInt(45),
-        "Gender": parseInt(1),
-        "Dependent_count": parseInt(3),
-        "Education_Level": parseInt(3),
-        "Marital_Status": parseInt(1),
-        "Income_Category": parseInt(4),
-        "Card_Category": parseInt(1),
-        "Months_on_book": parseInt(39),
-        "Total_Relationship_Count": parseInt(5),
-        "Months_Inactive_12_mon": parseInt(1),
-        "Contacts_Count_12_mon": parseInt(3),
-        "Credit_Limit": parseInt(12345),
-        "Total_Revolving_Bal": parseInt(1244),
-        "Avg_Open_To_Buy": parseInt(12345),
-        "Total_Amt_Chng_Q4_Q1": parseFloat(1.335),
-        "Total_Trans_Amt": parseInt(1144),
-        "Total_Trans_Ct": parseInt(42),
-        "Total_Ct_Chng_Q4_Q1": parseFloat(1.625),
-        "Avg_Utilization_Ratio": parseFloat(0.061),
+    // let customer = {
+    //     "Customer_Age": parseInt(45),
+    //     "Gender": parseInt(1),
+    //     "Dependent_count": parseInt(3),
+    //     "Education_Level": parseInt(3),
+    //     "Marital_Status": parseInt(1),
+    //     "Income_Category": parseInt(4),
+    //     "Card_Category": parseInt(1),
+    //     "Months_on_book": parseInt(39),
+    //     "Total_Relationship_Count": parseInt(5),
+    //     "Months_Inactive_12_mon": parseInt(1),
+    //     "Contacts_Count_12_mon": parseInt(3),
+    //     "Credit_Limit": parseInt(12345),
+    //     "Total_Revolving_Bal": parseInt(1244),
+    //     "Avg_Open_To_Buy": parseInt(12345),
+    //     "Total_Amt_Chng_Q4_Q1": parseFloat(1.335),
+    //     "Total_Trans_Amt": parseInt(1144),
+    //     "Total_Trans_Ct": parseInt(42),
+    //     "Total_Ct_Chng_Q4_Q1": parseFloat(1.625),
+    //     "Avg_Utilization_Ratio": parseFloat(0.061),
 
-    }
+    // }
 
     console.log(customer);
     console.log(customer["Customer_Age"]);
